ede7113f6b90e548a6f52e357246ed6d6cc11468
Ricardo Bachmann <ricardo.bachmann@icloud.com>
2025-12-12 20:51:51 +0100

    feat: implement landing page layout with flexbox and BEM

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./index.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+    <main>
+      <header className="header">
+        <div className="header__container">
+          <h2 className="header__title">Ricardo Bachmann</h2>
+          <p className="header__subtitle">
+            multi-disciplinary cartographer & researcher.
+          </p>
+        </div>
+      </header>
+      <aside className="sidebar">
+        <div className="sidebar__container">
+          <p className="sidebar__text">
+            Visualizing geo-social conflicts. Mapping where technology,
+            atmosphere & matter become political.
+          </p>
+        </div>
+      </aside>
+      <section className="hero">
+        <h1 className="hero__title">
+          <span>SENSING,</span>
+          <span>ATMOSPHERE,</span>
+          <span>MATTER.</span>
+        </h1>
+      </section>
+      <footer className="footer">
+        <p className="footer__text">
+          Currently under contrcution, feel free to reach out @
+          <a href="mailto:ricardobachmann@mailbox.org" className="footer__link">
+            ricardobachmann@mailbox.org
+          </a>
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      </footer>
+    </main>
+  );
 }
 
-export default App
+export default App;
