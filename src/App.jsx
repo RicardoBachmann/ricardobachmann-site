758d3ca97be653ef19b99ae355b0a50929211660
Ricardo Bachmann <ricardo.bachmann@icloud.com>
2025-12-13 20:18:42 +0100

    feat: add color system - CSS needs cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
         <div className="header__container">
           <h2 className="header__title">Ricardo Bachmann</h2>
           <p className="header__subtitle">
-            multi-disciplinary cartographer & researcher.
+            Multi-disciplinary Cartographer & Researcher.
           </p>
         </div>
       </header>
@@ -27,12 +27,7 @@ function App() {
         </h1>
       </section>
       <footer className="footer">
-        <p className="footer__text">
-          Currently under contrcution, feel free to reach out @
-          <a href="mailto:ricardobachmann@mailbox.org" className="footer__link">
-            ricardobachmann@mailbox.org
-          </a>
-        </p>
+        <button className="footer__text">INFO</button>
       </footer>
     </main>
   );
