2d7cd65eec7ce20abea9521b9a2c3ea25d5d65e9
Ricardo Bachmann <ricardo.bachmann@icloud.com>
2025-12-16 20:02:28 +0100

    feat:refactor: redesign mobile layout with hero section and improved spacing

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,52 @@ import "./index.css";
 function App() {
   return (
     <main>
-      <header className="header">
-        <div className="header__container">
-          <h2 className="header__title">Ricardo Bachmann</h2>
-          <p className="header__subtitle">
-            Multi-disciplinary Cartographer & Researcher.
-          </p>
-        </div>
-      </header>
-      <aside className="sidebar">
-        <div className="sidebar__container">
-          <p className="sidebar__text">
-            Visualizing geo-social conflicts. Mapping where technology,
-            atmosphere & matter become political.
-          </p>
-        </div>
-      </aside>
       <section className="hero">
         <h1 className="hero__title">
-          <span>SENSING,</span>
-          <span>ATMOSPHERE,</span>
-          <span>MATTER.</span>
+          <span>Sensing,</span>
+          <span>Atmosphere,</span>
+          <span>Matter.</span>
         </h1>
+        <h2 className="hero__name">RICARDO BACHMANN</h2>
       </section>
+      <section className="content">
+        <div className="content__text">
+          <p>
+            Visualizing geo-social conflicts.
+            <br /> Mapping where technology, atmosphere & matter become
+            political.
+          </p>
+          <p>
+            I am a Multi-disciplinary Cartographer & Researcher. Observing how
+            technological systems and environmental phenomena establish dynamic
+            networks in moments of uncertainty and change, because in stabile
+            situations the networks are invisible.Observing how technological
+            systems and environmental phenomena establish dynamic networks in
+            moments of uncertainty and change, because in stabile situations the
+            networks are invisible.{" "}
+          </p>
+        </div>
+        <section className="content__detail">
+          <div className="content__detail_contact">
+            <h3>[Contact]</h3>
+
+            <p>
+              Based in <br />
+              Hamburg, GER
+            </p>
+
+            <p>
+              For Job, Collaboration or <br />
+              say hi:
+            </p>
+
+            <a href="mailto:ricardobachmann@mailbox.org">
+              ricardobachmann@mailbox.org
+            </a>
+          </div>
+        </section>
+      </section>
+      <aside className="sidebar"></aside>
       <button className="info-btn">[ about ]</button>
     </main>
   );
