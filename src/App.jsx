b124d973a23a1266183915c977a41fa72ebf6a15
Ricardo Bachmann <ricardo.bachmann@icloud.com>
2025-12-17 16:21:10 +0100

    feat: implement desktop grid layout with 5 columns and navigation alignment

--- src/App.jsx
+++ src/App.jsx
@@ -7,27 +7,34 @@ function App() {
       <nav className="desktop-nav">
         <span className="desktop-nav__name">Ricardo Bachmann</span>
         <div className="desktop-nav__links">
-          <a className="desktop-nav__link">[ Contact ]</a>
-          <a className="desktop-nav__link">[ What i do ]</a>
+          <a className="desktop-nav__link">
+            <span className="bracket">[</span> Contact{" "}
+            <span className="bracket">]</span>
+          </a>
+          <a className="desktop-nav__link">
+            <span className="bracket">[</span> What i do{" "}
+            <span className="bracket">]</span>
+          </a>
         </div>
       </nav>
 
+      <section className="hero">
+        <h1 className="hero__title">
+          <span>Sensing,</span>
+          <span>Atmosphere,</span>
+          <span>Matter.</span>
+        </h1>
+        <h2 className="hero__name">Ricardo Bachmann</h2>
+      </section>
       <main>
-        <section className="hero">
-          <h1 className="hero__title">
-            <span>Sensing,</span>
-            <span>Atmosphere,</span>
-            <span>Matter.</span>
-          </h1>
-          <h2 className="hero__name">Ricardo Bachmann</h2>
-        </section>
-        <section className="content">
-          <div className="content__text">
+        {/* Column 1 */}
+        <section className="grid-column">
+          <div className="grid-column__content">
             <p>
               Visualizing geo-social conflicts.
-              <br /> Mapping where technology, atmosphere & matter become
-              political.
-            </p>
+              <br /> Mapping where technology, atmosphere & matter <br />
+              become political.
+            </p>{" "}
             <p>
               I am a Multi-disciplinary Cartographer & Researcher. Observing how
               technological systems and environmental phenomena establish
@@ -38,33 +45,37 @@ function App() {
               stabile situations the networks are invisible.{" "}
             </p>
           </div>
-          <section className="content__detail">
-            <div className="content__detail_contact">
-              <h3>
-                <span className="bracket">[</span>
-                Contact
-                <span className="bracket">]</span>
-              </h3>
-
-              <p>
-                Based in <br />
-                Hamburg, GER
-              </p>
-
-              <p>
-                For Job, Collaboration or <br />
-                say hi:
-              </p>
-
-              <a href="mailto:ricardobachmann@mailbox.org">
-                ricardobachmann@mailbox.org
-              </a>
-            </div>
-          </section>
         </section>
-        <aside className="sidebar"></aside>
-        <button className="info-btn">[ about ]</button>
+
+        {/* Column 2 */}
+        <section className="grid-column">
+          <div className="grid-column__content">
+            <p>
+              Based in <br />
+              Hamburg, GER
+            </p>
+            <p>
+              For Job, Collaboration or <br />
+              say hi:
+            </p>
+            <a href="mailto:ricardobachmann@mailbox.org">
+              ricardobachmann@
+              <br />
+              mailbox.org
+            </a>
+          </div>
+        </section>
+
+        {/* Column 3 */}
+        <section className="grid-column grid-column--empty"></section>
+        {/* Column 4 */}
+        <section className="grid-column grid-column--empty"></section>
+        {/* Column 5 */}
+        <section className="grid-column grid-column--empty"></section>
       </main>
+      <footer className="footer">
+        <p>Impressum ©2025 All Rights Reserved.</p>
+      </footer>
     </>
   );
 }
